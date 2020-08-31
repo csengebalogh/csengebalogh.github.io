fd9068d4d196ea3a30351f2a22cf3677cb9f70dc
csengebalogh <csenge.balogh1214@gmail.com>
2020-08-31 08:25:57 +0200

    Updates

---
+++ precache-manifest.b9eb6aff6a5f034c53211376026501f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bab8c8d6dc6793f2b122fe59b3141009",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b4643213f6d15391892",
+    "url": "/static/css/main.4adae061.chunk.css"
+  },
+  {
+    "revision": "b43e40e74326efd49226",
+    "url": "/static/js/2.ee5a24b8.chunk.js"
+  },
+  {
+    "revision": "e33bd41e3cbb3743b58dfbc09e761595",
+    "url": "/static/js/2.ee5a24b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b4643213f6d15391892",
+    "url": "/static/js/main.ab21e3ca.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "7126d65353efa665f28371df72108e85",
+    "url": "/static/media/footer.7126d653.svg"
+  }
+]);
